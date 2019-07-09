5ea63812214c437712d42fd99ab4ea73a2c94c98
Julian Eckhardt <julian@eckhardt.io>
2019-07-09 18:34:55 +0200

    Prettier, ESLint + Better Diffs + Bugfixes
    - Installed Prettier & ESLint (Airbnb)
    - Prettier fixes
    - Installed Cheerio
    - Better Diff (w/ cheerio; now comparing hash of all site text)
    - Deleted `getUrls` require
    - Made list command work (again)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 website-watchdog - a telegram bot for website monitoring
-Copyright (c) 2018  Julian Eckhardt
+Copyright (c) 2018 – 2019 Julian Eckhardt (www.eckhardt.io)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of version 2 of the GNU General Public License
@@ -16,22 +16,20 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-const sqlite = require('sqlite');
-const Telegraf = require('telegraf');
-const Telegram = require('telegraf/telegram');
-const Promise = require('bluebird');
-const axios = require('axios');
-const moment = require('moment')
-const SHA256 = require('crypto-js/sha256');
-const normalizeUrl = require('normalize-url');
-const schedule = require('node-schedule');
-
-// TODO: this can be deleted via npm… Still breaks stuff though…
-const getUrls = require('get-urls');
+const sqlite = require('sqlite'); // License: MIT
+const Telegraf = require('telegraf'); // License: MIT
+const Telegram = require('telegraf/telegram'); // License: MIT
+const Promise = require('bluebird'); // License: MIT
+const axios = require('axios'); // License: MIT
+const moment = require('moment'); // License: MIT
+const SHA256 = require('crypto-js/sha256'); // License: MIT
+const normalizeUrl = require('normalize-url'); // License: MIT
+const schedule = require('node-schedule'); // License: MIT
+const cheerio = require('cheerio'); // License: MIT
 
 //
 // CONFIG
-//=============
+// =============
 const config = require('./config.json');
 
 const bot = new Telegraf(config.token);
@@ -39,7 +37,7 @@ const telegram = new Telegram(config.token);
 
 //
 // REPORT MODES
-//=============
+// =============
 
 // full report
 const REPORT_DEBUG = 0;
@@ -49,123 +47,96 @@ const REPORT_INFO = 1;
 const REPORT_ERROR = 2;
 
 //
-// SCHEDULES
-//=============
-
-// DAILY 04:30 (SYSTEM TIME UTC) BRIEFING
-schedule.scheduleJob('0 30 4 * * *', async function() {
-	await sendReports(REPORT_INFO);
-});
-
-// EVERY 20 MINUTE CHECK
-schedule.scheduleJob('0 */20 * * * *', async function() {
-	await sendReports(REPORT_ERROR);
-});
+// HELP DOCUMENT
+// ===================
+const help =
+	'website-watchdog v0.1.0\n\nCommands:\n/scan -> perform full scan\n/list -> list all watched sites\n/register <url> -> add new site\n/help -> this help document';
 
 //
-// COMMANDS
-//=============
+// INIT SEQUENCE
+// ===================
 
-bot.start((ctx) => startService(ctx));
-bot.help((ctx) => ctx.reply(help));
-
-bot.command("scan", async function(ctx) {
-	report = await createReport(REPORT_DEBUG);
-	ctx.reply(report);
-})
-
-bot.command("register", async function(ctx) {
-	// get message parts
-	messageEntities = ctx.message.entities;
-	messageText = ctx.message.text;
-
-	// throw away the command entity
-	messageEntities.splice(0,1); 
-
-	messageEntities.forEach(async function(entity){
-		if(entity.type === 'url'){
-			url = normalizeUrl(messageText.substr(entity.offset, entity.length));
-			const db = await dbPromise;
-			const selectUrl = await db.get('SELECT * FROM target_sites WHERE site_url = ?', url);
-			if(selectUrl){
-				ctx.reply(`target site ${url} is already registered`);
-			}
-			else{
-				response = await axios.get(url);
-				hash = SHA256(response);
-
-				db.run('INSERT INTO target_sites (site_url, response_hash) VALUES ($url, $hash)', {$url: url, $hash: hash})
-					.catch((err) => ctx.reply(`could not persist new target site ${url}\n: ${err}`))
-					.then(ctx.reply(`successfully registered ${url}`));
-			}
-		}
-	})
-});
+const dbPromise = Promise.resolve()
+	.then(() => sqlite.open('./database.sqlite', { Promise }))
+	.then(db => db.migrate())
+	.catch(err => console.error(err.stack))
+	.then(console.log('connected to database.sqlite'))
+	.then(console.log('starting to poll for messages - startup sequence completed'))
+	.finally(() => bot.startPolling());
 
 //
 // HELPER FUNCTIONS
-//===================
+// ===================
 
-listWatchedSites = async function(){
+async function listWatchedSites() {
 	const db = await dbPromise;
-	const sites = db.get('SELECT * FROM target_sites');
-	message = "Currently watched sites:\n\n"
+	const sites = await db.all('SELECT * FROM target_sites');
+	let message = 'Currently watched sites:\n\n';
 
 	sites.forEach(site => {
-		message += (site.site_url + "\n");
+		message += `${site.site_url}\n`;
 	});
 
-	return messages;
+	return message;
 }
 
-startService = async function (ctx) {
+async function startService(ctx) {
 	const db = await dbPromise;
-	try{
-		db.run('INSERT OR REPLACE INTO users (id, first_name) VALUES ($id, $firstName)', {$id: ctx.chat.id, $firstName: ctx.chat.first_name})
-	}
-	catch (err){
+	try {
+		db.run('INSERT OR REPLACE INTO users (id, first_name) VALUES ($id, $firstName)', {
+			$id: ctx.chat.id,
+			$firstName: ctx.chat.first_name
+		});
+	} catch (err) {
 		console.err(err.stack);
 	}
-	const user = await db.get('SELECT * FROM users WHERE id = ?', ctx.chat.id)
-	telegram.sendMessage(user.id, "Hello 👋")
+	const user = await db.get('SELECT * FROM users WHERE id = ?', ctx.chat.id);
+	telegram.sendMessage(user.id, 'Hello 👋');
 }
 
-createReport = async function(mode) {
+async function createReport(mode) {
 	// prepare the message
-	message = "";
-	testPassed = true;
+	let message = '';
+	let testPassed = true;
 
 	// get sites from database
 	// assuming small n here - might change to filter by user at some point
 	const db = await dbPromise;
 	const sites = await db.all('SELECT rowid, site_url, response_hash FROM target_sites');
 
-	if(sites.length !== 0){
-		const pMessageChunks = sites.map(async function (site) {
-			messageChunk = "";
-			response = await axios.get(site.site_url)
-				.catch((err) => {
-					messageChunk = (`❌ ${site.site_url} (HTTP Error)\n\n ${err}`);
-					testPassed = false;
-				});
-			response_hash_new = SHA256(response.data).toString();
+	if (sites.length !== 0) {
+		const pMessageChunks = sites.map(async site => {
+			let messageChunk = '';
+			const response = await axios.get(site.site_url).catch(err => {
+				messageChunk = `❌ ${site.site_url} (HTTP Error)\n\n ${err.toString()}`;
+				testPassed = false;
+			});
 
-			if (response_hash_new !== site.response_hash) {
-				messageChunk = (`⚠️ ${site.site_url} (content changed)\n`);
+			const $ = cheerio.load(response.data);
+			// use combined text contents of whole page as content and hash it
+			const responseHashNew = SHA256($.text()).toString();
+
+			if (responseHashNew !== site.response_hash) {
+				messageChunk = `⚠️ ${site.site_url} (content changed)\n`;
 				testPassed = false;
 
-				await db.run('UPDATE target_sites SET response_hash = $response_hash WHERE rowid = $rowid', {$rowid: site.rowid, $response_hash: response_hash_new})
+				await db
+					.run('UPDATE target_sites SET response_hash = $response_hash WHERE rowid = $rowid', {
+						$rowid: site.rowid,
+						$response_hash: responseHashNew
+					})
 					.catch(err => console.log(err));
-			}
-			else if (mode === REPORT_DEBUG) {
-				messageChunk = (`✅ ${site.site_url}\n`);
+			} else if (mode === REPORT_DEBUG) {
+				messageChunk = `✅ ${site.site_url}\n`;
 			}
 			return messageChunk;
 		});
 
 		const messageChunks = await Promise.all(pMessageChunks);
-		if(messageChunks){
-			messageChunks.forEach(chunk => message += chunk);
+		if (messageChunks) {
+			messageChunks.forEach(chunk => {
+				message += chunk;
+			});
 		}
 
 		if (testPassed && mode === REPORT_INFO) {
@@ -174,44 +145,85 @@ createReport = async function(mode) {
 		if ((!testPassed && mode === REPORT_ERROR) || mode === REPORT_DEBUG) {
 			message += `\n\ntimestamp: ${moment().format('DD.MM.YY HH:mm zz')}\n`;
 		}
-	}
-	else {
-		message = "no target sites found";
+	} else {
+		message = 'no target sites found';
 	}
 	return message;
 }
 
-sendReports = async function(mode) {
+async function sendReports(mode) {
 	const message = await createReport(mode);
 
-	if (message !== "") {
+	if (message !== '') {
 		const db = await dbPromise;
 		const users = await db.all('SELECT * FROM users;');
 
-		if(users.length !== 0) {
-			users.map(async function(user) {
-				telegram.sendMessage(user.id, message)
-					.catch((err) => {
-						console.log(err);
-					})
+		if (users.length !== 0) {
+			users.map(async user => {
+				telegram.sendMessage(user.id, message).catch(err => {
+					console.log(err);
+				});
 			});
 		}
 	}
 }
 
 //
-// INIT SEQUENCE
-//===================
+// COMMANDS
+// =============
 
-const dbPromise = Promise.resolve()
-	.then(() => sqlite.open('./database.sqlite', { Promise }))
-	.then(db => db.migrate())
-	.catch((err) => console.error(err.stack))
-	.then(console.log("connected to database.sqlite"))
-	.then(console.log("starting to poll for messages - startup squence completed"))
-	.finally(() => bot.startPolling());
+bot.start(ctx => startService(ctx));
+bot.help(ctx => ctx.reply(help));
+
+bot.command('scan', async ctx => {
+	const report = await createReport(REPORT_DEBUG);
+	ctx.reply(report);
+});
+
+bot.command('register', async ctx => {
+	// get message parts
+	const messageEntities = ctx.message.entities;
+	const messageText = ctx.message.text;
+
+	// throw away the command entity
+	messageEntities.splice(0, 1);
+
+	messageEntities.forEach(async entity => {
+		if (entity.type === 'url') {
+			const url = normalizeUrl(messageText.substr(entity.offset, entity.length));
+			const db = await dbPromise;
+			const selectUrl = await db.get('SELECT * FROM target_sites WHERE site_url = ?', url);
+			if (selectUrl) {
+				ctx.reply(`target site ${url} is already registered`);
+			} else {
+				const response = await axios.get(url);
+				const hash = SHA256(response);
+
+				db.run('INSERT INTO target_sites (site_url, response_hash) VALUES ($url, $hash)', {
+					$url: url,
+					$hash: hash
+				})
+					.catch(err => ctx.reply(`could not persist new target site ${url}\n: ${err}`))
+					.then(ctx.reply(`successfully registered ${url}`));
+			}
+		}
+	});
+});
+
+bot.command('list', async ctx => {
+	ctx.reply(await listWatchedSites());
+});
 
 //
-// HELP DOCUMENT
-//===================
-help = "website-watchdog v0.1.0\n\nCommands:\n/scan -> perform full scan\n/list -> list all watched sites\n/register <url> -> add new site\n/help -> this help document";
+// SCHEDULES
+// =============
+
+// DAILY 04:30 (SYSTEM TIME UTC) BRIEFING
+schedule.scheduleJob('0 30 4 * * *', async () => {
+	await sendReports(REPORT_INFO);
+});
+
+// EVERY 20 MINUTE CHECK
+schedule.scheduleJob('0 */20 * * * *', async () => {
+	await sendReports(REPORT_ERROR);
+});
